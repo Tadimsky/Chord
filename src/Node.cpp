5d9af42c991fd86235d68e0aad18ed8540937a18
Jonno Schmidt <jonathan.schmidt@duke.edu>
2015-03-16 11:36:23 -0400

    Added src folder and Node class

---
+++ src/Node.cpp
@@ -0,0 +1,18 @@
+/*
+ * Node.cpp
+ *
+ *  Created on: Mar 16, 2015
+ *      Author: jonno
+ */
+
+#include "Node.h"
+
+Node::Node() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Node::~Node() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Node.h
@@ -0,0 +1,17 @@
+/*
+ * Node.h
+ *
+ *  Created on: Mar 16, 2015
+ *      Author: jonno
+ */
+
+#ifndef SRC_NODE_H_
+#define SRC_NODE_H_
+
+class Node {
+public:
+	Node();
+	virtual ~Node();
+};
+
+#endif /* SRC_NODE_H_ */
