d0b1e8df7cf5fd5c664e5b57bcc5c38b467dafdc
Jonno Schmidt <jonno@schmidtfam.us>
2015-03-20 02:36:49 -0400

    Added message formatting

--- src/Node.cpp
+++ src/Node.cpp
@@ -13,12 +13,13 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include "utils/csapp.h"
+#include <iostream>
+#include <sstream>
 
 using namespace std;
 
-//struct sockaddr_in address;
+//	struct sockaddr_in address;
 //	socklen_t length;
-//
 //	getpeername(file_descriptor, (sockaddr*)&address, &length);
 
 Node::Node(std::string ip_addr, int port) : Node(0, ip_addr, port) {
@@ -26,18 +27,30 @@ Node::Node(std::string ip_addr, int port) : Node(0, ip_addr, port) {
 
 Node::Node(int file_descriptor, std::string ip_addr, int port) {
 	myFD = file_descriptor;
+	if (myFD > 0) {
+		myRIOBuffer = shared_ptr<RIOBuffered>(new RIOBuffered(myFD));
+	}
+
 	myIPAddress = ip_addr;
 	myPort = port;
 	myKey = Chord::hashKey(myIPAddress + ":" + to_string(myPort));
 }
 
 Node::~Node() {
-	// TODO Auto-generated destructor stub
 }
 
 bool Node::Connect() {
 	myFD = Open_clientfd(myIPAddress.c_str(), myPort);
 	if (myFD > 0) {
+		myRIOBuffer = shared_ptr<RIOBuffered>(new RIOBuffered(myFD));
+
+		// other node sends hello message
+		myRIOBuffer->readLine();
+		// node's identity
+		string message = myRIOBuffer->readLine();
+
+		message = "Node " + this->toString();
+		this->send(&message);
 		return true;
 	}
 	return false;
@@ -47,12 +60,109 @@ unsigned int Node::getKey() {
 	return myKey;
 }
 
-void Node::processCommunication() {
+std::string Node::readLine() {
+	return myRIOBuffer->readLine();
+}
+
+size_t Node::send(const std::string* message) {
+	return RIO::writeString(myFD, message);
+}
+
+void Node::processCommunication(RIOBuffered* rio) {
+	myRIOBuffer = std::shared_ptr<RIOBuffered>(rio);
+
 	stringstream stream;
 	stream << "Hello " << std::hex << myKey << "\n";
 	string message(stream.str());
-	RIO::writep(myFD, (void*)message.c_str(), message.length());
-	while (true) {
+	this->send(&message);
 
+	while (true) {
+		// block until it reads a line.
+		message = this->readLine();
+		stringstream str(message);
+		string command;
+		str >> command;
+
+		// SET Queries - all specific to the instance of Chord
+		if (command.compare("GET") == 0) {
+			// get next part of get request
+			str >> command;
+
+			if (command.compare("SUCCESSOR") == 0) {
+				int index;
+				str >> index;
+				if (Chord::getInstance()->mySuccessors.size() > index - 1) {
+					string response = Chord::getInstance()->mySuccessors[index - 1].toString();
+					this->send(&response);
+				}
+				else {
+					this->send(&(Node::NOT_FOUND));
+				}
+			}
+			else if (command.compare("PREDECESSOR") == 0) {
+					int index;
+					str >> index;
+			}
+			else if (command.compare("INFO") == 0) {
+				string s = Chord::getInstance()->toString();
+				this->send(&s);
+			}
+
+
+		}
+		// Get Queries - all specific to the instance of Chord
+		else if (command.compare("SET") == 0) {
+
+		}
+		// Find queries
+		else if (command.compare("FIND") == 0) {
+			str >> command;
+			if (command.compare("SUCCESSOR") == 0) {
+				string key;
+				str >> key;
+				// find the closest predecessor
+				// return successor of that
+			}
+		}
+		// Find queries
+		else if (command.compare("SEARCH") == 0) {
+			str >> command;
+			if (command.compare("SUCCESSOR") == 0) {
+				string key;
+				str >> key;
+				// perform iterative lookup of successor for a key
+				// use FIND SUCCESSOR on nodes to get the actual value
+				// find predecessor to the id
+				// return successor of that
+			}
+		}
+		// Find queries
+		else if (command.compare("REQUEST") == 0) {
+			// Join
+		}
+		else {
+			cout << "Unknown Request: " << message;
+		}
+	}
+}
+
+std::string Node::toString() {
+	stringstream result;
+	result << hex << myKey << " " << myIPAddress << ":" << dec << myPort << endl;
+	return result.str();
+}
+
+std::shared_ptr<Node> Node::getSuccessor(int index) {
+	if (!this->isConnected()) {
+		this->Connect();
+	}
+}
+
+std::shared_ptr<Node> Node::getPredecessor(int index) {
+}
+
+bool Node::isConnected() {
+	if (myFD == 0) {
+		return false;
 	}
 }
--- src/Node.h
+++ src/Node.h
@@ -9,15 +9,24 @@
 #define SRC_NODE_H_
 
 #include <string>
+#include <memory>
+#include "utils/csapp.h"
 
 class Node {
 private:
+	const std::string NOT_FOUND = "Item Not Found\n";
 	std::string myIPAddress;
 	int myPort;
 
 	int myFD;
 
+	std::shared_ptr<RIOBuffered> myRIOBuffer;
+
 	unsigned int myKey;
+
+	char buffer[RIO_BUFSIZE];
+	std::string readLine();
+	size_t send(const std::string* message);
 
 public:
 	/**
@@ -35,7 +44,14 @@ public:
 
 	unsigned int getKey();
 
-	void processCommunication();
+	void processCommunication(RIOBuffered* rio);
+
+	std::shared_ptr<Node> getSuccessor(int index = 1);
+	std::shared_ptr<Node> getPredecessor(int index = 1);
+
+	std::string toString();
+
+	bool isConnected();
 };
 
 #endif /* SRC_NODE_H_ */
