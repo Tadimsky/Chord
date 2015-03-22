23a8a2aaca160a1815378c3e9e6d769f3855b575
Jonno Schmidt <jonno@schmidtfam.us>
2015-03-22 00:44:16 -0400

    Process LEAVE message on Node

--- src/Node.cpp
+++ src/Node.cpp
@@ -24,7 +24,8 @@ using namespace std;
 //	socklen_t length;
 //	getpeername(file_descriptor, (sockaddr*)&address, &length);
 
-Node::Node(std::string ip_addr, int port) : Node(0, ip_addr, port) {
+Node::Node(std::string ip_addr, int port) :
+		Node(0, ip_addr, port) {
 }
 
 Node::Node(int file_descriptor, std::string ip_addr, int port) {
@@ -93,6 +94,8 @@ void Node::processCommunication(std::shared_ptr<RIOBuffered> rio) {
 	while (true) {
 		// block until it reads a line.
 		message = this->readLine();
+		cout << message;
+
 		if (message.length() == 0) {
 			// we have been shutdown by the other side. Bye!
 			return;
@@ -113,42 +116,53 @@ void Node::processCommunication(std::shared_ptr<RIOBuffered> rio) {
 
 				if (chord->Successors.size() > index - 1) {
 					response = chord->Successors[index - 1]->toString();
-				}
-				else {
+				} else {
 					if (index == 1) {
 						// this happens when there are no successors because it's the only one.
 						response = chord->NodeInfo->toString();
-					}
-					else {
+					} else {
 						response = Node::NOT_FOUND;
 					}
 				}
 				this->send(&response);
-			}
-			else if (command.compare("PREDECESSOR") == 0) {
-					size_t index;
-					str >> index;
-					string response;
+			} else if (command.compare("PREDECESSOR") == 0) {
+				size_t index;
+				str >> index;
+				string response;
 
-					if (chord->Predecessors.size() > index - 1) {
-						response = chord->Predecessors[index - 1]->toString();
+				if (chord->Predecessors.size() > index - 1) {
+					response = chord->Predecessors[index - 1]->toString();
+				} else {
+					if (index == 1) {
+						// this happens when there are no successors because it's the only one.
+						response = chord->NodeInfo->toString();
+					} else {
+						response = Node::NOT_FOUND;
 					}
-					else {
-						if (index == 1) {
-							// this happens when there are no successors because it's the only one.
-							response = chord->NodeInfo->toString();
-						}
-						else {
-							response = Node::NOT_FOUND;
-						}
-					}
-					this->send(&response);
-			}
-			else if (command.compare("INFO") == 0) {
+				}
+				this->send(&response);
+			} else if (command.compare("SUCCESSORS") == 0) {
+				stringstream s;
+
+				for (size_t i = 0; i < chord->Successors.size(); ++i) {
+					s << i + 1<< ": " << chord->Successors[i]->toString();
+				}
+
+				string msg = s.str();
+				this->send(&msg);
+			} else if (command.compare("PREDECESSORS") == 0) {
+				stringstream s;
+
+				for (size_t i = 0; i < chord->Predecessors.size(); ++i) {
+					s << i + 1 << ": " << chord->Predecessors[i]->toString();
+				}
+
+				string msg = s.str();
+				this->send(&msg);
+			} else if (command.compare("INFO") == 0) {
 				string s = chord->toString();
 				this->send(&s);
-			}
-			else if (command.compare("RANGE") == 0) {
+			} else if (command.compare("RANGE") == 0) {
 				auto range = chord->getRange();
 				stringstream msg_s;
 				msg_s << hex << get<0>(range) << " " << hex << get<1>(range) << endl;
@@ -177,8 +191,7 @@ void Node::processCommunication(std::shared_ptr<RIOBuffered> rio) {
 					// we don't want to reset up a link to the node.
 					// they just told us to set it up.
 					chord->setSuccessor(index, node, false);
-				}
-				else {
+				} else {
 					chord->setPredecessor(index, node, false);
 				}
 			}
@@ -217,15 +230,12 @@ void Node::processCommunication(std::shared_ptr<RIOBuffered> rio) {
 						string msg = node->toString();
 						myRIOBuffer->writeLine(&msg);
 						break;
-					}
-					else {
+					} else {
 						cout << "We have " << hex << node->getKey() << ". We are looking for " << hex << key << endl;
 						// get the next best attempt
 						node = node->FindSuccessor(key);
 					}
 				}
-
-
 
 //				if (node->getKey() == Chord::getInstance()->NodeInfo->getKey()) {
 //					// we just return ourselves
@@ -242,13 +252,15 @@ void Node::processCommunication(std::shared_ptr<RIOBuffered> rio) {
 		// Find queries
 		else if (command.compare("REQUEST") == 0) {
 			// Join
-		}
-		else {
+		} else {
 			if (command.compare(Node::EXIT_MSG) == 0) {
 				// quit and close
 				string goodbye = "Goodbye Friend!\n";
 				myRIOBuffer->writeLine(&goodbye);
 				return;
+			}
+			if (command.compare("LEAVE") == 0) {
+				chord->LeaveRing();
 			}
 			cout << "Unknown Request: " << message;
 		}
@@ -297,7 +309,7 @@ bool Node::isConnected() {
 
 std::shared_ptr<Node> Node::SearchSuccessor(unsigned int key) {
 	if (!this->isConnected()) {
-			this->Connect();
+		this->Connect();
 	}
 	stringstream s;
 	s << "SEARCH SUCCESSOR " << hex << key << endl;
@@ -312,7 +324,7 @@ std::shared_ptr<Node> Node::SearchSuccessor(unsigned int key) {
 
 std::shared_ptr<Node> Node::FindSuccessor(unsigned int key) {
 	if (!this->isConnected()) {
-			this->Connect();
+		this->Connect();
 	}
 	stringstream s;
 	s << "FIND SUCCESSOR " << hex << key << endl;
@@ -327,7 +339,7 @@ std::shared_ptr<Node> Node::FindSuccessor(unsigned int key) {
 
 void Node::setSuccessor(Node* node, int index) {
 	if (!this->isConnected()) {
-			this->Connect();
+		this->Connect();
 	}
 	stringstream s;
 	s << "SET SUCCESSOR " << index << " " << node->toString();
@@ -347,7 +359,7 @@ void Node::setPredecessor(Node* node, int index) {
 
 std::tuple<unsigned int, unsigned int> Node::getRange() {
 	if (!this->isConnected()) {
-			this->Connect();
+		this->Connect();
 	}
 	stringstream s;
 	s << "GET RANGE" << endl;
